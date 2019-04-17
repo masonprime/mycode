0fae91e922714c5886be1046f6d4506477befbad
Tim Mason <masonprime@gmail.com>
2019-04-17 17:15:02 -0400

    log count parser

---
+++ keystonecounter.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+counter = 0
+
+#with open("keystone.common.wsgi", "r") as keyfile:
+#    mylines = keyfile.readlines()
+#    for line in mylines:
+#        if "- - - - -] Authorization failed" in line:
+#            # counter = counter + 1
+#           counter += 1
+
+# for line in open("keystone.common.wsgi"):
+#    if "- - - - -] Authorization failed" in line:
+#        counter = counter + 1
+
+with open("keystone.common.wsgi") as keyfile:
+    for line in keyfile:
+        if "- - - - -] Authorization failed" in line:
+            counter = counter + 1
+
+print("The number of failed logins is: ", counter)
+
+
---
+++ mybash.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "hello world" >> itworked.txt
+echo "Tim made this" >> timfile.txt
+
